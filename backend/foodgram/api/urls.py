b6bec2f273c9576e41c50aa9104d19d9771110a2
SmirEV <ev_smir@mail.ru>
2023-08-03 23:20:24 +0300

    As in webinar - 35m40s

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,12 +1,15 @@
 ﻿from rest_framework import routers
 from django.urls import path, include
 
-from api.views import index
+from api.views import index, CustomUserViewSet
 
 
 router = routers.DefaultRouter()
+router.register('users', CustomUserViewSet)
 
 urlpatterns = [
     path('index', index),
-    path('auth/', include('djoser.urls.authtoken'))
+#    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('', include(router.urls)),
 ]
