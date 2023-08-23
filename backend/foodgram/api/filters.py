1c4fc1a94e5f036a78a7b925349acad8e144b264
SmirEV <ev_smir@mail.ru>
2023-08-23 16:38:29 +0300

    With get_is_in_shopping_cart

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -44,7 +44,7 @@ class RecipeFilter(FilterSet):
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(in_shopping_cart__user=self.request.user)
+            return queryset.filter(cooker__user=self.request.user)
         return queryset
 
 
